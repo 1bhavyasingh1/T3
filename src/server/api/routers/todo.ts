851984ea8d79540c586383a9e4b4dbfaaf943827
Bhavya Singh <91753565+1bhavyasingh1@users.noreply.github.com>
2023-06-01 22:57:52 +0530

    Initial Commit

---
+++ src/server/api/routers/todo.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import {
+    createTRPCRouter,
+    publicProcedure,
+    protectedProcedure,
+} from "~/server/api/trpc";
+
+export const todoRouter = createTRPCRouter({
+    all: protectedProcedure.query(async ({ ctx }) => {
+        const todos = await ctx.prisma.todo.findMany({
+            where: {
+                userId: ctx.session.user.id,
+            },
+        });
+        return todos.map()
+        return [
+            {
+                id: 'fake',
+                text: 'fake text',
+                done: false,
+
+            }
+            {
+                id: 'fake2',
+                text: 'fake text2',
+                done: true,
+            }
+        ]
+    }),
+
+    create: protectedProcedure.input().mutation(async ({ ctx }) => {
+
+    })
+});
